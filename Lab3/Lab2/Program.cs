6c08f75c4a60e6786fd6aacfbb1a13930e51ed8f
codycmartin <codycmartin89@gmail.com>
2019-10-03 15:17:43 -0700

    Lab2/Lab3 Demo

---
+++ Lab3/Lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SportsCar myCar = new SportsCar();
+            myCar.PetName = "my beat up sienna";
+            Console.WriteLine("My car: {0}", myCar.getPetName());
+        }
+    }
+}
